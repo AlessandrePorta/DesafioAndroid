dea3c918ae58a3da7cc08b2e72ee36340dffa56f
Alessandre <ale_junior97@hotmail.com>
2021-11-05 15:07:01 -0300

    filters implemented, cleartextbutton, layout changed

--- app/src/main/java/com/example/desafioandroid2/main/MainActivity.kt
+++ app/src/main/java/com/example/desafioandroid2/main/MainActivity.kt
@@ -3,19 +3,16 @@ package com.example.desafioandroid2.main
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
 import com.example.desafioandroid2.R
+import com.example.desafioandroid2.main.model.Item
 import com.example.desafioandroid2.main.model.ListaItems
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.TextView
-import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,41 +24,43 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         init()
+
     }
 
     private fun init() {
 
         showLoading(true)
         mainViewModel.repos.observe(this) { setupList(it) }
-        mainViewModel.repos.observe(this) { searchValue(it)}
-        //mainViewModel.repos.observe(this) { filterList(it) }
+        mainViewModel.repos.observe(this) { searchValue() }
         mainViewModel.fetchRepos()
 
     }
 
     private fun setupList(response: ListaItems) {
-        showLoading(false)
-        val rv_item_list = findViewById<RecyclerView>(R.id.rv_item_list)
 
-        rv_item_list.adapter = MainAdapter(response, this)
-        rv_item_list.addItemDecoration(
-            DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
-        )
+        showLoading(false)
+        rvDivider()
+        initAdapter(response)
+        filterLanguageList(response)
+        filterModuleList(response)
+        cleanFilters(response)
     }
 
-    fun searchValue(response: ListaItems){
+    private fun initAdapter(response: MutableList<Item>) {
+        val rv_item_list = findViewById<RecyclerView>(R.id.rv_item_list)
+
+        mainAdapter = MainAdapter(
+            response,
+            this
+        )
+
+        rv_item_list.adapter = mainAdapter
+    }
+
+    private fun searchValue() {
+        showLoading(false)
         val rv_item_list = findViewById<RecyclerView>(R.id.rv_item_list)
         val search = findViewById<EditText>(R.id.et_search)
-
-        mainAdapter =
-            MainAdapter(
-                response,
-                this
-            )
-        rv_item_list.apply {
-            setHasFixedSize(true)
-            adapter = mainAdapter
-        }
 
         search.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {}
@@ -77,44 +76,99 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
-//    fun filterList(listaItems : ListaItems){
-//        lateinit var spinner = findViewById<Spinner>(R.id.sp_list)
-//        val listFilter = findViewById<TextView>(R.id.tv_picked)
-//        val listaAux : MutableList<String> = mutableListOf()
-//
-//        listaItems.forEach{
-//            listaItems.first().resource.language_id.forEach {
-//                if(!listaItems.first().resource.language_id.contains(it)){
-//                    listaAux.add(it.toString())
-//                }
-//            }
-//        }
-//        val adapter : ArrayAdapter<CharSequence> = ArrayAdapter.createFromResource(
-//            this,
-//            R.array.teste,
-//            android.R.layout.simple_spinner_item
-//        )
-//
-//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-//        spinner.adapter = adapter
-//
-//        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-//            override fun onItemSelected(
-//                parent: AdapterView<*>?,
-//                view: View,
-//                position: Int,
-//                id: Long
-//            ) {
-//                listFilter.text = parent?.getItemAtPosition(position).toString()
-//                Toast.makeText(parent?.context, listFilter.toString(),  Toast.LENGTH_SHORT).show()
-//            }
-//
-//            override fun onNothingSelected(parent: AdapterView<*>?) {}
-//        }
-//    }
+    private fun filterLanguageList(response: ListaItems) {
+        showLoading(false)
+        val rv_item_list = findViewById<RecyclerView>(R.id.rv_item_list)
+        val actv_language = findViewById<AutoCompleteTextView>(R.id.actv_language)
 
-    fun showLoading(isLoaded : Boolean){
+        filterLanguages(response)
+
+        actv_language.setOnItemClickListener { adapterView, view, i, l ->
+            clearSearchText()
+
+            val listaAux = mainAdapter.items.filter {
+                it.resource.language_id.contains(actv_language.text)
+            }.toMutableList()
+
+            rv_item_list.adapter = MainAdapter(listaAux, this@MainActivity)
+            rv_item_list.adapter?.notifyDataSetChanged()
+
+        }
+    }
+
+    private fun filterLanguages(response: ListaItems) {
+        val actv = findViewById<AutoCompleteTextView>(R.id.actv_language)
+        val lista: ArrayList<String> = arrayListOf()
+
+        response.forEach {
+            if (!lista.toString().contains(it.resource.language_id)) {
+                lista.add(it.resource.language_id)
+            }
+        }
+
+        val adapter = ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, lista)
+        actv.setAdapter(adapter)
+    }
+
+    private fun filterModuleList(response: ListaItems) {
+        showLoading(false)
+        val rv_item_list = findViewById<RecyclerView>(R.id.rv_item_list)
+        val actv_module = findViewById<AutoCompleteTextView>(R.id.actv_module)
+
+        filterModules(response)
+
+        actv_module.setOnItemClickListener { adapterView, view, i, l ->
+            clearSearchText()
+
+            var listaAux = mainAdapter.items.filter {
+                it.resource.module_id.contains(actv_module.text)
+            }.toMutableList()
+            rv_item_list.adapter = MainAdapter(listaAux, this@MainActivity)
+            rv_item_list.adapter?.notifyDataSetChanged()
+        }
+    }
+
+    private fun filterModules(response: ListaItems) {
+        val actv = findViewById<AutoCompleteTextView>(R.id.actv_module)
+        val lista: ArrayList<String> = arrayListOf()
+
+        response.forEach {
+            if (!lista.toString().contains(it.resource.module_id)) {
+                lista.add(it.resource.module_id)
+            }
+        }
+
+        val adapter = ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, lista)
+        actv.setAdapter(adapter)
+    }
+
+    private fun cleanFilters(response: ListaItems) {
+        val rv_item_list = findViewById<RecyclerView>(R.id.rv_item_list)
+        val cleanFilter = findViewById<TextView>(R.id.tv_clean_filters)
+
+        cleanFilter.setOnClickListener {
+            rv_item_list.adapter = MainAdapter(response, this@MainActivity)
+            rv_item_list.adapter?.notifyDataSetChanged()
+        }
+    }
+
+    private fun clearSearchText() {
+        val search = findViewById<EditText>(R.id.et_search)
+        search.setText("")
+    }
+
+    private fun rvDivider() {
+        val rv_item_list = findViewById<RecyclerView>(R.id.rv_item_list)
+        rv_item_list.addItemDecoration(
+            DividerItemDecoration(this, DividerItemDecoration.VERTICAL)
+        )
+    }
+
+    private fun showLoading(isLoaded: Boolean) {
         val loading = findViewById<ProgressBar>(R.id.pb_loading)
+        val mainActivity = findViewById<ConstraintLayout>(R.id.main_activity2)
+        mainActivity.isVisible = !isLoaded
         loading.isVisible = isLoaded
     }
+
 }
